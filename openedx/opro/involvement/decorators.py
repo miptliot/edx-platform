3bb1f8dc97a14c7b9b825353cbb092fac9017c6a
dm-v <strannik772@gmail.com>
2019-01-29 23:28:15 +0300

    DEV-367 - Параметр "вовлеченность" для edx-based онлайн-курсов Университета

--- openedx/opro/involvement/decorators.py
+++ openedx/opro/involvement/decorators.py
@@ -2,14 +2,15 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.utils.http import urlquote
 from opaque_keys.edx.keys import CourseKey
+from student.models import CourseEnrollment
 from .utils import ask_enable_involvement
 
 
 def check_involvement(func):
     def wrapper(self, request, course_id, chapter=None, section=None, position=None):
-        if ask_enable_involvement(course_id, request.user):
+        course_key = CourseKey.from_string(course_id)
+        if CourseEnrollment.is_enrolled(request.user, course_key) and ask_enable_involvement(course_id, request.user):
             next_page = urlquote(request.get_full_path())
-            course_key = CourseKey.from_string(course_id)
             redirect_url = reverse('involvement:check-involvement', kwargs={'course_id': course_key})
             redirect_url = ''.join([redirect_url, '?next=', next_page])
             return HttpResponseRedirect(redirect_url)
